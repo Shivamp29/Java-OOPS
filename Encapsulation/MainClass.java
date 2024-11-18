136e906b755cb61d43adc778e70e8ba9ca5bbc40
Shivam Patil <122163220+Shivamp29@users.noreply.github.com>
2024-11-18 22:32:49 +0530

    Update MainClass.java

--- Encapsulation/MainClass.java
+++ Encapsulation/MainClass.java
@@ -1,1 +1,24 @@
+class Instagram
+{  
+    // defining Private data fields
+    private  int id ;
+    private String name ;
+    private String email ;
+    private String password ;
 
+    // defining a constructor 
+    public Instagram( int id , String name , String email , String password )
+    {  
+      this.id = id ;
+      this.name = name ;
+      this.email = email ;
+      this.password = password ;
+    }
+    
+}
+
+class Mainclass
+{  
+
+}
+  
