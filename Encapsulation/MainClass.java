6bd5ad3f0ca55959442fd25817dba9946465924d
Shivam Patil <122163220+Shivamp29@users.noreply.github.com>
2024-12-02 23:08:31 +0530

    Update MainClass.java

--- Encapsulation/MainClass.java
+++ Encapsulation/MainClass.java
@@ -14,11 +14,53 @@ class Instagram
       this.email = email ;
       this.password = password ;
     }
-    
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+    public string getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public void setEmail(String email)
+    {
+        this.eamil = eamil;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
 }
 
 class Mainclass
 {  
+    public static void main(string[] args)
+    {
+        Instagram i = new Instagram(1 , "Kiran" , "kiran@gmail.com" , "kiran123");
 
+        System.out.println(" id : " +i.getId());
+        System.out.println(" name : " +i.getName());
+        System.out.println(" Email : " +i.getEmail());
+        System.out.println(" Password : " +i.getPassword());
+        
+    }
 }
   
