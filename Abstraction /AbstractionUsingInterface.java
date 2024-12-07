81ec025af2697b8fc330fe7db95cfb76c0fc07d6
Shivam Patil <122163220+Shivamp29@users.noreply.github.com>
2024-12-07 18:45:41 +0530

    Create AbstractionUsingInterface.java

---
+++ Abstraction /AbstractionUsingInterface.java
@@ -0,0 +1,59 @@
+package com.jsp.Abstraction;
+
+interface Vehical
+{
+	void start();
+	void move();
+	void stop();
+	
+}
+
+class Bike implements Vehical
+{
+	public void start()
+	{
+		System.out.println("Bike starting by kick...");
+	}
+	public void move()
+	{
+		System.out.println("putting gear to move the bike...");
+	}
+	public void stop()
+	{
+		System.out.println("applying break to stop....");
+	}
+}
+
+class Car implements Vehical
+{
+	public void start() {
+		System.out.println("starting by key...");
+	}
+	
+	public void move() {
+		System.out.println("applying acceleration to move...");
+	}
+	
+	public void stop() {
+		
+		System.out.println("applying break to stop..");
+	}
+}
+public class AbstractionUsingInterface
+{
+	public static void main(String[] args) {
+		
+		Bike b = new Bike();
+
+    b.start();
+		b.move();
+		b.stop();
+    
+		Car c = new Car();
+	
+		c.start();
+		c.move();
+		c.stop();
+	
+	}
+}
